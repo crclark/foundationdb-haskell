4a2de8f0b71df8e5b87164a133641c581925d7ba
Connor Clark <connor.r.clark@gmail.com>
2019-01-18 18:27:17 -0800

    Export defaultConfig

--- src/FoundationDB.hs
+++ src/FoundationDB.hs
@@ -16,6 +16,7 @@ module FoundationDB (
   , runTransaction
   , runTransaction'
   , TransactionConfig (..)
+  , defaultConfig
   , runTransactionWithConfig
   , runTransactionWithConfig'
   , cancel
