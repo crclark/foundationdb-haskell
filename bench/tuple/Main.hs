d0945067287cf06a70bfed7a978f654293cc058d
Connor Clark <connor.r.clark@gmail.com>
2019-02-01 19:14:17 -0800

    Benchmark encodeTupleElems

--- bench/tuple/Main.hs
+++ bench/tuple/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import FoundationDB.Versionstamp
 import FoundationDB.Layer.Tuple.Internal
 
 import Gauge.Main
@@ -10,4 +11,8 @@ main =
   [ bgroup "bisectSize"
     [ bench "bisectSize small" (nf bisectSize 1025)
     , bench "bisectSize medium" (nf bisectSize 123456789012)
-    , bench "bisectSize large"  (nf bisectSize 1234567890123454555)]]
+    , bench "bisectSize large"  (nf bisectSize 1234567890123454555)]
+
+  , bgroup "encodeTupleElems"
+    [ bench "IncompleteVS"
+            (nf encodeTupleElems [IncompleteVS (IncompleteVersionstamp 1)])]]
