e5f2b15dcf69b29550aa23d745c8184c840950bb
Connor Clark <connor.r.clark@gmail.com>
2022-01-09 16:33:33 -0500

    Change CI download URI
    See https://github.com/apple/foundationdb/issues/6154

--- ci/install-deps.sh
+++ ci/install-deps.sh
@@ -3,8 +3,8 @@
 FDB_VER=${1}
 
 sudo apt install python
-wget https://www.foundationdb.org/downloads/${FDB_VER}/ubuntu/installers/foundationdb-clients_${FDB_VER}-1_amd64.deb
-wget https://www.foundationdb.org/downloads/${FDB_VER}/ubuntu/installers/foundationdb-server_${FDB_VER}-1_amd64.deb
+wget https://github.com/apple/foundationdb/releases/download/${FDB_VER}/foundationdb-clients_${FDB_VER}-1_amd64.deb
+wget https://github.com/apple/foundationdb/releases/download/${FDB_VER}/foundationdb-server_${FDB_VER}-1_amd64.deb
 sudo dpkg -i foundationdb-clients_${FDB_VER}-1_amd64.deb
 # 6.3.x seems to require us to make this directory
 sudo mkdir -p /var/lib/foundationdb/data
