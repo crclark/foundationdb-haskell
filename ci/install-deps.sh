eea561039d2372ff39da8b0c55125a3d034c5f9b
Connor Clark <connor.r.clark@gmail.com>
2023-08-25 15:10:29 -0400

    oops

--- ci/install-deps.sh
+++ ci/install-deps.sh
@@ -7,8 +7,8 @@ FDB_VER=${1}
 sudo apt install python2
 wget https://github.com/apple/foundationdb/releases/download/${FDB_VER}/foundationdb-clients_${FDB_VER}-1_amd64.deb
 wget https://github.com/apple/foundationdb/releases/download/${FDB_VER}/foundationdb-server_${FDB_VER}-1_amd64.deb
-sudo apt install foundationdb-clients_${FDB_VER}-1_amd64.deb
+sudo apt install ./foundationdb-clients_${FDB_VER}-1_amd64.deb
 # 6.3.x seems to require us to make this directory
 sudo mkdir -p /var/lib/foundationdb/data
-sudo apt install foundationdb-server_${FDB_VER}-1_amd64.deb
+sudo apt install ./foundationdb-server_${FDB_VER}-1_amd64.deb
 sudo apt-get install c2hs
